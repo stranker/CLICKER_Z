9a212f23feb614b6303eaca15e5aafc5dd4b5c14
Unknown <dani_mrx93@hotmail.com>
2018-03-06 22:48:04 -0300

    ADD SPAWN CONTROL

--- Object/Global.gd
+++ Object/Global.gd
@@ -3,6 +3,12 @@ extends Node
 var player_pos = Vector2()
 var zombie_count = 0
 var zombie_killed = 0
+const MAX_COUNT_ZOMBIES = 200
+var ZOMBI_LIST = []
+var DAMAGE = 0
+var GOLD = 0
+var EXP = 0
 
 func _ready():
+	DAMAGE = 1
 	pass
