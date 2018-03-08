875573102862c30a8c864a21b1e65135471fb689
Unknown <dani_mrx93@hotmail.com>
2018-03-07 22:49:32 -0300

    Add bullet pistol y script weapon

---
+++ Object/Weapon.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var cost = 0
+var dmg = 0
+
+func _ready():
+	pass
+
+func get_cost():
+	return cost
+	
+func get_dmg():
+	return dmg
+	
+	
+func set_cost(val):
+	cost = val
+	
+func set_dmg(val):
+	dmg = val
+	
+
+func shoot():
+	pass
