d3c87ac29566b07f4296cff087cba9e1371c7c34
Unknown <dani_mrx93@hotmail.com>
2018-03-12 13:25:42 -0300

    Add weapon construct and shop

--- Object/Global.gd
+++ Object/Global.gd
@@ -1,14 +1,14 @@
 extends Node
 
 var player_pos = Vector2()
-var zombie_count = 0
 var zombie_killed = 0
 const MAX_COUNT_ZOMBIES = 200
 var ZOMBI_LIST = []
-var DAMAGE = 0
 var GOLD = 0
 var EXP = 0
+var GOLD_POS = Vector2()
+var WEAPON_LIST = []
+var curr_weapon = null
 
 func _ready():
-	DAMAGE = 1
 	pass
--- Object/Weapon.gd
+++ Object/Weapon.gd
@@ -1,7 +1,11 @@
 extends Node
 
 var cost = 0
-var dmg = 0
+var damage = 0
+var shop_texture = "test"
+var weapon_name = "test"
+var bullet = preload("res://Object/Bullet.tscn")
+var buyed = false
 
 func _ready():
 	pass
@@ -10,15 +14,45 @@ func get_cost():
 	return cost
 	
 func get_dmg():
-	return dmg
-	
+	return damage
 	
 func set_cost(val):
 	cost = val
 	
 func set_dmg(val):
-	dmg = val
-	
+	damage = val
+
+func set_shop_texture(tex):
+	shop_texture = str(tex)
+
+func get_shop_texture():
+	return shop_texture
+
+func set_weapon_name(nam):
+	weapon_name = str(nam)
+
+func get_weapon_name():
+	return weapon_name
+
+func create_weapon(wname,cst,dmg,stext):
+	weapon_name = wname
+	cost = cst
+	damage = dmg
+	shop_texture = stext
+	pass
+
+func set_buy(con):
+	buyed = con
+
+func get_buy():
+	return buyed
 
 func shoot():
+	if Global.ZOMBI_LIST.size()>0:
+		var zombi = Global.ZOMBI_LIST[0]
+		var b = bullet.instance()
+		get_tree().root.call_deferred("add_child",b)
+		b.set_direction(zombi.global_position)
+		b.global_position = Global.player_pos
+		b.set_damage(get_dmg())
 	pass
