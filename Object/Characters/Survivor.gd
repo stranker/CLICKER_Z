8fba00cf19acfb03d3859f3ebabfcc80eaeb516f
Unknown <dani_mrx93@hotmail.com>
2018-03-15 22:49:19 -0300

    Add buildings!

---
+++ Object/Characters/Survivor.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var survivor_name = ""
+var info = ""
+var level = 0
+
+func _ready():
+	pass
+
+func create_survivor(nam,inf,lvl):
+	survivor_name = nam
+	info = inf
+	level = lvl
+	pass
--- Object/Global.gd
+++ Object/Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# Antiguas variables
 var player_pos = Vector2()
 var zombie_killed = 0
 const MAX_COUNT_ZOMBIES = 200
@@ -9,6 +10,12 @@ var EXP = 0
 var GOLD_POS = Vector2()
 var WEAPON_LIST = []
 var curr_weapon = null
+
+# Variables de alimento
+var curr_food = 0
+var curr_water = 0
+var MAX_FOOD = 0
+var MAX_WATER = 0
 
 func _ready():
 	pass
