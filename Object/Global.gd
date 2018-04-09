922c1a2adb2e65f334c13dcac94ebf53a3d2767d
Unknown <dani_mrx93@hotmail.com>
2018-04-08 22:30:01 -0300

    Adding Globalpos diamond

--- Object/Global.gd
+++ Object/Global.gd
@@ -1,12 +1,9 @@
 extends Node
 
-# Antiguas variables
-var player_pos = Vector2()
-var zombie_killed = 0
+# Zombies
 const MAX_COUNT_ZOMBIES = 500
 var ZOMBI_LIST = []
-var EXP = 0
-var curr_weapon = null
+
 # Gold y Diamonds
 var diamonds = 0
 var diamond_pos = Vector2()
@@ -15,6 +12,8 @@ var GOLD = 0
 
 #Achieves
 var gold_obtained = 0
+var zombie_killed = 0
+var EXP = 0
 
 # Variables de Deposit
 var curr_food = 0
@@ -32,6 +31,7 @@ var MAX_SURVIVOR = 0
 var SURVIVOR_LIST = []
 var player
 var player_income = 0
+var player_pos = Vector2()
 
 # Weapons
 var WEAPON_LIST = []
@@ -62,3 +62,13 @@ func remove_resources(food,water,gold):
 	GOLD -= gold
 	limit_supplies()
 	pass
+
+func number_converter(number):
+	var converted = ""
+	if number >= 1000000:
+		converted = str(number/1000000)+"."+str((number%1000000)/10000)+"M"
+	elif number >= 1000:
+		converted = str(number/1000)+"."+str((number%1000)/10)+"k"
+	else:
+		converted = str(number)
+	return converted
